2e1a2f6274fc11f0a39802ef708294c6ac0834d3
Vinicius Barbosa <viniciusds.barbosa@hotmail.com>
2021-04-30 00:40:33 -0300

    atualizando as aula_javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "galenae"
+var n1 = 5
+var n2 = 3
+var frase = "japão é o melhor time do mundo"
+// alert(nome + " tem " + idade + " anos ")
+// alert(idade + idade2)
+console.log(nome)
+console.log(n1 * n2)
+console.log(frase.toUpperCase())
+// alert(frase.replace("japão", "Brasil"))
